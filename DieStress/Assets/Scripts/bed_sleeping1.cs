8f73b48caef3bc8dd0ec4da9e8d90e9d7007cd5c
ItaThePita <aitamari15@gmail.com>
2021-06-01 16:59:24 +0300

    Interactions, Scenes and buildings

---
+++ DieStress/Assets/Scripts/bed_sleeping1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bed_sleeping1 : MonoBehaviour
+{
+    public GameObject sleepText;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            sleepText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                SceneManager.LoadScene(sceneBuildIndex: 2);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            sleepText.SetActive(false);
+        }
+    }
+}
